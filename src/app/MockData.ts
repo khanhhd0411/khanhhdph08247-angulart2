c70fd7f76f74c9310d86ba1ff534ce55558ff548
khanhhd0411 <kienkakax1@gmail.com>
2020-03-16 21:59:58 +0700

    5 files modified, 1 added, 1 deleted

---
+++ src/app/MockData.ts
@@ -0,0 +1,15 @@
+import {Product} from './Product'
+export const data : Product[] = [
+  {
+    id: 1,
+    name: "Iteam 1",
+    price: 25.99,
+    desc: "Mo ta san pham"
+  },
+  {
+    id: 2,
+    name: "Iteam 2",
+    price: 25.99,
+    desc: "Mo ta san pham"
+  }
+];
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Product} from '../Product';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -11,9 +11,4 @@ export class ProductComponent implements OnInit {
 
   ngOnInit() {
   }
-product : Product ={
-  name: 'Iteam 1',
-  price: 25.99,
-  desc: 'Mo ta san pham'
-}
 }
