dd3245faa616f2445260ac8ae4d9ebce56257a3d
khanhhd0411 <kienkakax1@gmail.com>
2020-03-18 15:05:25 +0700

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { CategoryComponent } from './category/category.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductComponent } from './product/product.component';
 import { ManagerProductComponent } from './manager-product/manager-product.component';
+import { DetailComponent } from './detail/detail.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, SlideComponent, CategoryComponent, ProductListComponent, ProductComponent, ManagerProductComponent ],
+  declarations: [ AppComponent, HelloComponent, SlideComponent, CategoryComponent, ProductListComponent, ProductComponent, ManagerProductComponent, DetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
